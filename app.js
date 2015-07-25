0cf8b207d99c790e6e063852ff06dbb8cf185ae8
Sarmad Hashmi <shash034@uottawa.ca>
2015-07-25 19:50:45 -0400

    Only pipe to response if file is sucessfully opened, and created semester dates config file

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var fs = require('fs');
 
 var express = require('express');
 var multer = require('multer')	
-var parserFactory = require('./parsers/main.js');
 var winston = require('winston');
 var async = require('async');
+
+var parserFactory = require('./parsers/main.js');
+var semesterConfig = require('./config/semesters.json');
 // Add logging
 winston.add(winston.transports.File, { filename: __dirname + '/logs/main.log' });
 
@@ -29,29 +31,28 @@ app.post('/upload', function(req, res) {
 	}
 
 	var university = req.body.university;
-	var semester = req.body.semester;
-
-	//Default Values for Dates
-	var start_date = [2015,8,7];
-	var end_date = new Date(2015,11,08, 23, 30, 0);
-
-	if (semester === "fall"){
-	start_date = [2015,8,7];
-	end_date = new Date(2015,11,08, 23, 30, 0);
-	}
-	if (semester === "winter"){
-		start_date = [2016,0,7];
-		end_date = new Date(2016,3,21, 23, 30, 0);
-	}
+	var semester = req.body.semester;	
 	var filename = req.files.file.name;
-	winston.info('Getting parser for ' + university);
-	winston.info('Getting the timetable for the ' + semester + " Semester");
+
+	var config, start_date, end_date;
+	if (semesterConfig[university] && semesterConfig[university][semester]) {
+		config = semesterConfig[university][semester];
+		start_date = config['start_date'];
+		end_date = config['end_date'];		
+		end_date = new Date(end_date[0], end_date[1], end_date[2], 23, 30, 0);
+	} else {
+		var msg = 'Invalid university or semester provided';
+		winston.error(msg);
+		return res.status(404).send(msg);
+	}
+
+	winston.info('Getting parser for ' + university + ' for semester  ' + semester);	
 	async.waterfall([
 		function(callback) {
 			parserFactory.getParser(university, callback);
 		},
-		function(convertToCal, callback) {			
-			convertToCal(tempFolder + filename, start_date,end_date, icsFolder, callback);
+		function(convertToCal, callback) {						
+			convertToCal(tempFolder + filename, start_date, end_date, icsFolder, callback);
 		}
 	], function(err, fileName) {
 		if (err) {
@@ -60,12 +61,15 @@ app.post('/upload', function(req, res) {
 		} else if (!fileName) {
 			var msg = 'Invalid university provided or parser not found: ' + university;
 			winston.error(msg);
-			res.status(404).send(msg);
+			res.status(404).send(msg);	
 		} else {		
-			winston.info('Piping file to response: ' + fileName)
+			winston.info('Piping file ' +  fileName + ' to response.')
 			res.attachment('timetable.ics');
-			var filestream = fs.createReadStream(icsFolder + fileName);		
-			filestream.pipe(res);		
+			console.log(icsFolder + fileName);
+			var filestream = fs.createReadStream(icsFolder + fileName);	
+			filestream.on('readable', function() {
+				filestream.pipe(res);		
+			});
 		}				 
 	});	
 });
@@ -73,5 +77,5 @@ app.post('/upload', function(req, res) {
 
 app.use(express.static(__dirname + '/public'));
 app.listen(3000, function() {	
-	console.log("Started server at http://localhost:3000.");
+	winston.info("Started server at http://localhost:3000.");
 });
--- public/js/core.js
+++ public/js/core.js
@@ -12,17 +12,30 @@ main.config(['$routeProvider', function($routeProvider) {
 
 
 main.controller('MainController', ['$scope', '$http', 'Upload', function($scope, $http, Upload) {
-	$scope.universities = [{name: 'Example', value: 'example'},{name: 'McMaster', value: 'mcmaster'}, {name:'UOttawa', value: 'uottawa'}];
-	$scope.semesterList = [{ name: 'Fall 2015', value: 'fall' }, { name: 'Winter 2016', value: 'winter' }];
-	$scope.upload = function(files, university,semester) {
+	$scope.universities = [
+		{name: 'Example', value: 'example'},
+		{name: 'McMaster', value: 'mcmaster'}, 
+		{name:'UOttawa', value: 'uottawa'}
+	];
+	$scope.semesterList = [
+		{ name: 'Fall 2015', value: 'fall' }, 
+		{ name: 'Winter 2016', value: 'winter' },
+		{ name: 'Summer 2016', value: 'summer' }, 
+		{ name: 'Spring 2016', value: 'spring' }, 
+		{ name: 'Summer/Spring 2016', value: 'springsummer' }
+	];
+
+	$scope.upload = function(files) {
 		if (files && files.length > 0) {
 			Upload.upload({
 					url: '/upload',
 					file: files[0],
-					fields: {'university': $scope.university, 'semester': $scope.semester}
+					fields: {
+						'university': $scope.university, 
+						'semester': $scope.semester
+					}
 				}) 
-				.progress(function(evt) {
-					var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+				.progress(function(evt) {					
 		            $scope.message = 'Uploading and converting';
 				})
 				.success(function(data) {
