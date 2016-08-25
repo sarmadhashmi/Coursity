7b0693fb91042e6db309aad8c80ab629a623d4ea
Sarmad Hashmi <sarmadhashmi@gmail.com>
2016-08-25 00:43:38 -0400

    Fix bug identified by Abe where daylight savings was causing an issue due to inconsistent Date object

--- parsers/ics-builder.js
+++ parsers/ics-builder.js
@@ -8,33 +8,34 @@ function dayOfWeekAsInteger(day) {
 function buildICS(timetable) {
   var builder = icalToolkit.createIcsFileBuilder();
   builder.tzid = 'america/toronto';
-  builder.method = 'REQUEST';
+  builder.method = 'PUBLISH';
   builder.calname = 'Coursity';
   for (i = 0; i < timetable.length; i++) {
     var curr = timetable[i];
     var day_index = dayOfWeekAsInteger(curr['day']);
-    var class_start = new Date(curr['semester_start']['year'], 
+    var class_start = new Date(Date.UTC(curr['semester_start']['year'], 
                  curr['semester_start']['month'], 
                  curr['semester_start']['day'],
                  curr['start_time']['hour'],
-                 curr['start_time']['minute'], 0, 0);
+                 curr['start_time']['minute'], 0, 0));
     class_start.setDate(class_start.getDate() - class_start.getDay() + day_index);
     
-    var class_end = new Date(curr['semester_start']['year'], 
+    var class_end = new Date(Date.UTC(curr['semester_start']['year'], 
                  curr['semester_start']['month'], 
                  curr['semester_start']['day'],
                  curr['end_time']['hour'],
-                 curr['end_time']['minute'], 0, 0);
+                 curr['end_time']['minute'], 0, 0));
     class_end.setDate(class_end.getDate() - class_end.getDay() + day_index);
 
-    var semester_end = new Date(curr['semester_end']['year'], 
+    var semester_end = new Date(Date.UTC(curr['semester_end']['year'], 
         curr['semester_end']['month'], 
-        curr['semester_end']['day'], 24, 60, 0, 0);
+        curr['semester_end']['day'], 24, 60, 0, 0));
 
     builder.events.push({
       start: class_start,
       end: class_end,
       summary: curr['course_code_faculty'] + " " + curr['course_code_number'] + " " + curr['course_name'],
+      floating: true,
       repeating: {
         freq: 'WEEKLY',
         byday: curr['day'].toUpperCase().substring(0,2),
